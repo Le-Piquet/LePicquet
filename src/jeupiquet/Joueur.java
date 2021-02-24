152111f8073a3247f1b54a857f8545857956e828
abour <abour@LAPTOP-HM99D02K.home>
2021-02-24 21:23:57 +0100

    fin méthode initialiserManche

--- src/jeupiquet/Joueur.java
+++ src/jeupiquet/Joueur.java
@@ -5,17 +5,19 @@
  */
 package jeupiquet;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author theobaptiste
  */
 public class Joueur {
 
-    private String main;
+    private ArrayList main;
     private String pseudo;
     private int score;
 
-    public Joueur(String main, String pseudo, int score) {
+    public Joueur(ArrayList main, String pseudo, int score) {
         this.main = main;
         this.pseudo = pseudo;
         this.score = score;
