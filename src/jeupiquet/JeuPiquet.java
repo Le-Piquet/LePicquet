24e8b1e497c494f6d132cc709fd73616126dc943
abour <abour@LAPTOP-HM99D02K.home>
2021-02-26 12:18:59 +0100

    fin méthode initialiserManche

--- src/jeupiquet/JeuPiquet.java
+++ src/jeupiquet/JeuPiquet.java
@@ -18,8 +18,20 @@ public class JeuPiquet {
      */
     public static void main(String[] args) {
         Jeu jeu = new Jeu();
-        jeu.initialiserManche();
-        Joueur joueur = new Joueur(, Dupont, 0);
+        ArrayList mains = jeu.initialiserManche();
+        int role = RandomUtils.nextInt(2);
+        if (role == 0){
+        }
+        
+        //On crée deux listes qui contiennent l'index de la carte dans la main du joueur. Ces cartes vont être ajoutés dans la liste des cartes écartées.
+        ArrayList <Integer> ecarteJ1 = new ArrayList();
+        ArrayList <Integer> ecarteJ2 = new ArrayList();
+        ecarteJ1.set(0, 1);
+        ecarteJ1.set(1, 5);
+        ecarteJ1.set(2, 11);
+        ecarteJ2.set(0, 3);
+        ecarteJ2.set(1, 8);
+        jeu.getJoueur1().ecarterCartes(ecarteJ1, jeu.getTalon());
+        jeu.getJoueur2().ecarterCartes(ecarteJ2, jeu.getTalon());
     }
-    
 }
